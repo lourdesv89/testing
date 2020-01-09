9d04d8786f97bac44f7ad00c58983618db8ff597
lourdesv89 <lourdes_velazquez@ucsb.edu>
2020-01-08 16:30:10 -0800

    loofahs is awesome

--- Testing.R
+++ Testing.R
@@ -1,2 +1,6 @@
 print("this file was created within Rstudio")
 print ("and now it lives on github")
+
+print("loofahs will learn how to program")
+print("loofahs will soon be the best programmer in the world")
+print("...with the best memory")
